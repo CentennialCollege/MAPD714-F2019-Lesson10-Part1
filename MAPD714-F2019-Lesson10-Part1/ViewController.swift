3046d4fed07800a66f66c749c77b4851ed970ddf
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2019-11-13 20:03:26 -0500

    updated

--- MAPD714-F2019-Lesson10-Part1/ViewController.swift
+++ MAPD714-F2019-Lesson10-Part1/ViewController.swift
@@ -1,18 +1,70 @@
-//
-//  ViewController.swift
-//  MAPD714-F2019-Lesson10-Part1
-//
-//  Created by Tom Tsiliopoulos on 2019-11-13.
-//  Copyright © 2019 CentennialCollege. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var lineFields:[UITextField]!
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+         let fileURL = self.dataFileURL()
+        
+        if (FileManager.default.fileExists(atPath: fileURL.path!)) {
+            if let array = NSArray(contentsOf: fileURL as URL) as? [String] {
+                for i in 0..<array.count {
+                    lineFields[i].text = array[i]
+                }
+            }
+        }
+        
+        let app = UIApplication.shared
+        
+
+        NotificationCenter.default.addObserver(self, selector: #selector(self.applicationWillResignActive(notification:)), name: UIApplication.willResignActiveNotification, object: app)
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let fileURL = self.dataFileURL()
+        
+        if (FileManager.default.fileExists(atPath: fileURL.path!)) {
+            if let array = NSArray(contentsOf: fileURL as URL) as? [String] {
+                for i in 0..<array.count {
+                    lineFields[i].text = array[i]
+                }
+            }
+        }
+        
+        let app = UIApplication.shared
+        
+
+        /*
+        NotificationCenter.default.addObserver(self, selector: #selector(self.applicationWillResignActive(notification:)), name: UIApplication.willResignActiveNotification, object: app)
+ */
+    }
+    
+     
+    @objc func applicationWillResignActive(notification: NSNotification) {
+        let fileURL = self.dataFileURL()
+        let array = (self.lineFields as NSArray).value(forKey: "text") as! NSArray
+        array.write(to: fileURL as URL, atomically: true)
+    }
+
+
+    
+    func dataFileURL() -> NSURL {
+        let urls = FileManager.default.urls(for:
+            .documentDirectory, in: .userDomainMask)
+        var url:NSURL?
+        // create a blank path
+        url = URL(fileURLWithPath: "") as NSURL?
+        do {
+            url = urls.first?.appendingPathComponent("data.plist") as NSURL?
+        }
+        catch {
+            print("Error is \(error)")
+        }
+        return url!
     }
 
 
